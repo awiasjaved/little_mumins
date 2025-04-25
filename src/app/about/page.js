ec34977b0612165d54aad35f9018c8f83263b3bd
amir16sp <hafizamirsaeed906@gmail.com>
2025-04-25 22:37:04 +0500

    ok

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import TopNav from '../TopBar/TopNav';
+import Footer from '../Footer/FooterEnd';
+import bgImage from '../assets/images/baali.jpg'; // apni image ka path adjust karein
 
 export default function AboutPage() {
   return (
-    <div>
-      <TopNav />
-      <h1 className="text-3xl mt-8 text-center">About Us</h1>
-      {/* Add more about page content here */}
+    <>
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImage.src})` }}
+    >
+     
+      <div className="text-white text-center pt-32">
+        <h1 className="text-5xl font-bold">About Us</h1>
+        <p className="mt-4 text-xl">Welcome to our journey!</p>
+      </div>
+      
     </div>
+    <Footer/>
+    </>
   );
 }
