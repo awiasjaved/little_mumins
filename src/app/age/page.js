8f24dae98b08045c2c24fb3883e987552fd96012
Awais <muhammadawaisjaved388@gmail.com>
2025-04-26 10:50:28 +0500

    Age

---
+++ src/app/age/page.js
@@ -0,0 +1,26 @@
+import Footer from '../Footer/FooterEnd';
+import bgImage from '../assets/images/baali.jpg'; // apni image ka path adjust karein
+import Story from '../OurStory/Story';
+import Flowers from '../ShopNow/Flowers';
+const age = () => {
+  return (
+    <>
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImage.src})` }}
+    >
+     
+      <div className="text-white text-center pt-32">
+        <h1 className="text-5xl font-bold">About Us</h1>
+        <p className="mt-4 text-xl">Welcome to our journey!</p>
+      </div>
+      
+    </div>
+    <Story/>
+    <Flowers/>
+    <Footer/>
+    </>
+  );
+}
+
+export default age;
